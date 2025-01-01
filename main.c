f80b78dcb61ef2a79646eec40a830f09a4f3459d
Nader <87259389+Gorbekhan01@users.noreply.github.com>
2025-01-01 13:18:17 +0330

    Game menu added

--- main.c
+++ main.c
@@ -4,24 +4,38 @@
 #include <unistd.h>
 #include <string.h>
 
+typedef struct user{
+    char username[100];
+    char total_score[100];
+    char total_gold[100];
+    char total_finished_games[100];
+    char total_time[100];
+
+}user;
 
 void opening();
-void choosing_user();
-void new_user();
-void old_user();
+int choosing_user(char *username);
+int new_user(char *username);
+int old_user(char *username);
+int game_menu(char *username);
+int  leaderboard(struct user *current_user);
 
 int main() {
 
     start_color();
     setlocale(LC_ALL, "");
-    putenv("LINES=45");
-    putenv("COLUMNS=145");
     initscr();
     noecho();
     cbreak();
-
+    struct user current_user;
     opening();
-    choosing_user();
+    char username[50] = {};
+    choosing_user(username);
+    strcpy(current_user.username,username);
+    int choice = game_menu(username);
+    if(choice==0){
+        leaderboard(&current_user);
+    }
 
     endwin();
     return 0;
@@ -58,12 +72,11 @@ void opening() {
     refresh();
 }
 
-void choosing_user(){
+int choosing_user(char *username){
     initscr();
     keypad(stdscr, TRUE);
     curs_set(0);
     init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(1));
     int selected = 0;
     int key;
     int max_y, max_x;
@@ -98,29 +111,30 @@ void choosing_user(){
             case '\n':
                 clear();
                 if(selected == 0) {
-                   new_user();
+                    new_user(username);
+                    return 0;
                 }
                 else if(selected == 1) {
-                    old_user();
+                    old_user(username);
+                    return 0;
                 }
                 else if(selected == 2) {
-
+                    return 0;
                 }
                 refresh();
                 getch();
                 break;
         }
     }
+    refresh();
     attroff(COLOR_PAIR(1));
-
     endwin();
 }
 
-void old_user() {
+int old_user(char *username) {
     clear();
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(1));
     int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x);
     int center_y = max_y / 2 - 7;
@@ -137,7 +151,7 @@ void old_user() {
     mvprintw(center_y + 13, center_x-1, "(  /  )");
     mvprintw(center_y + 14, center_x-1, " \\(__)|");
     mvprintw(center_y + 14, center_x-1, " \\(__)|");
-    mvprintw(center_y + 18, center_x-10, "if you forget your password enter < forget > in password section!");
+    mvprintw(center_y + 18, center_x-20, "if you forget your password enter < forget > in password section!");
 
     mvprintw(center_y + 4, center_x, "◦ Username: ");
     mvprintw(center_y + 6, center_x, "◦ Password: ");
@@ -145,14 +159,13 @@ void old_user() {
     refresh();
 
     echo();
-    char username[50] = {};
     move(center_y + 4, center_x + 12);
+    curs_set(1);
     getstr(username);
     FILE* fptr = fopen("users.txt", "r");
     if(fptr == NULL) {
         FILE* fptr = fopen("users.txt", "w");
         refresh();
-        return;
     }
     char password[50];
     int len = 0;
@@ -164,10 +177,10 @@ void old_user() {
     rewind(fptr);
     while (fgets(tempi, 100, fptr) != NULL) {
         sscanf(tempi, "%s %s %s", temp_username, temp_password, temp_email);
-            if (strcmp(temp_username, username) == 0) {
-                break;
-            }
+        if (strcmp(temp_username, username) == 0) {
+            break;
         }
+    }
     refresh();
 
     while(1) {
@@ -197,17 +210,16 @@ void old_user() {
     }
 
     mvprintw(center_y + 12, center_x+9, ". . .                                     ");
-
     clear();
     refresh();
     getch();
 }
 
-void new_user() {
+int new_user(char *username) {
     clear();
     start_color();
+    curs_set(1);
     init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(1));
     int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x);
     int center_y = max_y / 2 - 7;
@@ -231,20 +243,17 @@ void new_user() {
     refresh();
 
     echo();
-    char username[50] = {};
     int temp = 0;
     FILE* fptr = fopen("users.txt", "r");
     if(fptr == NULL) {
         FILE* fptr = fopen("users.txt", "w");
         refresh();
-        return;
     }
     //taken username !!!!
     while (1) {
         move(center_y + 4, center_x + 12);
         clrtoeol();
         getstr(username);
-
         char temp_username[50];
         char temp_password[50];
         char temp_email[50];
@@ -307,4 +316,134 @@ void new_user() {
     clear();
     refresh();
     getch();
+}
+
+int game_menu(char *username){
+    initscr();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    refresh();
+    noecho();
+    cbreak();
+    clear();
+    start_color();
+    curs_set(1);
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
+    int center_y = max_y / 2 - 7;
+    int center_x = max_x / 2 - 15;
+    int selected =0;
+    int key;
+    while(1) {
+        clear();
+        attron(COLOR_PAIR(1) | A_BOLD);
+        mvprintw(center_y - 2, center_x-4, "══════════════════════════════");
+        attron(COLOR_PAIR(1) | A_BOLD | A_BLINK);
+        mvprintw(center_y, center_x, "    WELCOME  %s ! ", username);
+        attroff(COLOR_PAIR(1) | A_BOLD | A_BLINK);
+        mvprintw(center_y + 2, center_x-4, "══════════════════════════════");
+        attroff(COLOR_PAIR(1) | A_BOLD);
+        mvprintw(center_y+20,center_x , selected == 0 ? "[ start a new game ]" : "start a new game");
+        mvprintw(center_y+22, center_x-3, selected == 1 ? "[ continue your last Game ]" : "continue your last Game");
+        refresh();
+        key = getch();
+
+        switch(key) {
+            case KEY_UP:
+                selected--;
+                if(selected < 0) {
+                    selected = 1;
+                }
+                break;
+            case KEY_DOWN:
+                selected++;
+                if(selected > 1) {
+                    selected = 0;
+                }
+                break;
+            case '\n':
+                clear();
+                if(selected == 0) {
+                    return 0;
+                }
+                else if(selected == 1) {
+                    return 1;
+                }
+                refresh();
+                getch();
+                break;
+        }
+    }
+    refresh();
+    return 0;
+}
+
+int leaderboard(struct user *current_user) {
+    initscr();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    refresh();
+    noecho();
+    cbreak();
+    clear();
+
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
+    int center_y = max_y / 2 -10;
+    int center_x = max_x / 2 -40;
+
+    const int COL_WIDTH = 15;
+    mvprintw(center_y, center_x + 25, "=== LEADERBOARD ===");
+    mvprintw(center_y + 2, center_x, "%-10s %-15s %-15s %-15s %-15s %-15s",
+             "Rank", "Username", "Score", "Gold", "Games", "Time");
+    mvprintw(center_y + 3, center_x, "----------------------------------------------------------------");
+
+    FILE* fptr = fopen("leaderboard.txt", "r");
+    if (fptr == NULL) {
+        mvprintw(center_y + 5, center_x, "No leaderboard data available!");
+        refresh();
+        getch();
+        return 0;
+    }
+
+    int found = 0;
+    char rank[100];
+    char username1[100];
+    char total_score[100];
+    char total_gold[100];
+    char total_finished_games[100];
+    char total_time[100];
+    char tempi[100];
+
+    rewind(fptr);
+    while (fgets(tempi, 100, fptr) != NULL) {
+        sscanf(tempi, "%s %s %s %s %s %s", rank, username1, total_score, total_gold, total_finished_games, total_time);
+        if (strcmp(username1, current_user->username) == 0) {
+            found = 1;
+            strcpy(current_user->total_score, total_score);
+            strcpy(current_user->total_gold, total_gold);
+            strcpy(current_user->total_time, total_time);
+            strcpy(current_user->total_finished_games, total_finished_games);
+
+            mvprintw(center_y + 4, center_x, "%-10s %-15s %-15s %-15s %-15s %-15s",
+                     rank,
+                     current_user->username,
+                     current_user->total_score,
+                     current_user->total_gold,
+                     current_user->total_finished_games,
+                     current_user->total_time);
+            break;
+        }
+    }
+
+    if (!found) {
+        mvprintw(center_y + 5, center_x, "Player not found in leaderboard!");
+    }
+
+    fclose(fptr);
+    mvprintw(center_y + 30, center_x, "[ Press enter to start the game ]");
+    refresh();
+    getch();
+    return 0;
 }
